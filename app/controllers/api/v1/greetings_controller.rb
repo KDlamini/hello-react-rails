6f05804cbcb59c5dd87e3a614a65877b3160c208
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-03-30 12:13:17 +0200

    Fix linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,17 +1,17 @@
 module Api
-    module V1
-        class GreetingsController < ApplicationController
-            def index
-                greetings = Greeting.all
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        greetings = Greeting.all
 
-                render json: GreetingSerializer.new(greetings).serialized_json
-            end
+        render json: GreetingSerializer.new(greetings).serialized_json
+      end
 
-            def show
-                greeting = Greeting.find(params[:id])
+      def show
+        greeting = Greeting.find(params[:id])
 
-                render json: GreetingSerializer.new(greeting).serialized_json
-            end
-        end
+        render json: GreetingSerializer.new(greeting).serialized_json
+      end
     end
+  end
 end
